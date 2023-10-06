3eb03b8bb9cc34438604549ef49a68877c19905f
momen124 <momenhossny2004@gmail.com>
2023-10-06 04:45:18 -0700

    d

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,8 +19,7 @@ if (d > 5)
 printf("Last digit of %d is %d and is greater than 5\n", n, d);
 else if (d == 0)
 printf("Last digit of %d is %d and is 0\n", n, d);
-else 
+else
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, d);
-
 return (0);
 }
