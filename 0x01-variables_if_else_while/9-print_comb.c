bd6071a21edd8bf0fea696941af879b2e6cc3111
momen124 <55993326+momen124@users.noreply.github.com>
2023-10-07 01:55:52 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - main function
  *
@@ -7,12 +8,14 @@
 int main(void)
 {
 int i;
-for (i = 0 ; i < 10 ; i++)
+for (i = 0; i < 10; i++)
 {
 putchar(i + '0');
 if (i != 9)
+{
 putchar(',');
 putchar(' ');
+}
 }
 putchar('\n');
 return (0);
