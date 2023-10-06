3587c9fd16781e90aac2a9b0e8de72b368cd1149
momen124 <momenhossny2004@gmail.com>
2023-10-06 04:35:39 -0700

    d

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,19 +9,18 @@
  */
 int main(void)
 {
-    int n;
-    int d;
+int n;
+int d;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    d = n % 10;
-    
-    if (d > 5)
-        printf("%d is greater than 5\n", n);
-    else if (d == 0)
-        printf("%d is 0\n", n);
-    else
-        printf("%d is less than 6 and not 0\n", n);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+d = n % 10;
+if (d > 5)
+printf("%d is greater than 5\n", n);
+else if (d == 0)
+printf("%d is 0\n", n);
+else
+printf("%d is less than 6 and not 0\n", n);
 
-    return (0);
+return (0);
 }
