00cfa1a36646a4d1d9cd5c3f10dbd5a70ca89f58
momen124 <55993326+momen124@users.noreply.github.com>
2023-10-06 14:50:29 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,12 +4,12 @@
  *
  * Return: always 0
  */
-int main(void){
-	char so;
-	for(so = 'a' ; so <= 'z' ; so++){
-		putchar(so);
-	}
-
-	return(0);
-	}
+int main(void)
+{
+char so;
+for(so = 'a' ; so <= 'z' ; so++)
+putchar(so);
+putchar('\n');
+return(0);
+}
 
