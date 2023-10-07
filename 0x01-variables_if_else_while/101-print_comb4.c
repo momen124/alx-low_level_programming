16a92cfb6a59313216b1c9ecf286c15e70237282
momen124 <55993326+momen124@users.noreply.github.com>
2023-10-07 13:24:04 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,26 +7,27 @@
  */
 int main(void)
 {
-int d1 = 0, d2;
-while (d1 <=9)
-{
-d2 =0;
-while (d2 <= 9)
-{
-if (d1 != d2 && d1 < d2)
-{
-putchar(d1+48);
-putchar(d2+48);
-if (d2 + d2 != 17)
-{
-putchar(',');
-putchar(' ');
-}
-}
-d2++;
-}
-d1++;
-}
-putchar('\n');
-return (0);
+    int d1 = 0, d2;
+
+    while (d1 <= 9)
+    {
+        d2 = 0;
+        while (d2 <= 9)
+        {
+            if (d1 != d2 && d1 < d2)
+            {
+                putchar(d1 + '0');
+                putchar(d2 + '0');
+                if (d1 + d2 != 17)
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+            d2++;
+        }
+        d1++;
+    }
+    putchar('\n');
+    return (0);
 }
