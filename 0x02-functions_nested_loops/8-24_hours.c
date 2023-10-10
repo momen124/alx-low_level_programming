c9180f84f94937d60f3d362056d055cf7e19c4d1
momen124 <55993326+momen124@users.noreply.github.com>
2023-10-11 00:38:54 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,17 +7,17 @@
  */
 void jack_bauer(void)
 {
-    int min, h;
-    for (h = 0; h <= 23; h++)
-    {
-        for (min = 0; min <= 59; min++)
-        {
-            _putchar((h / 10) + '0');
-            _putchar((h % 10) + '0');
-            _putchar(':');
-            _putchar((min / 10) + '0');
-            _putchar((min % 10) + '0');
-            _putchar('\n');
-        }
-    }
+int min, h;
+for (h = 0; h <= 23; h++)
+{
+for (min = 0; min <= 59; min++)
+{
+_putchar((h / 10) + '0');
+_putchar((h % 10) + '0');
+_putchar(':');
+_putchar((min / 10) + '0');
+_putchar((min % 10) + '0');
+_putchar('\n');
+}
+}
 }
