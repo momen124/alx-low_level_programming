dc297e67e4635e370132137f1fe5efc3f3069043
momen124 <55993326+momen124@users.noreply.github.com>
2023-10-11 02:09:06 +0300

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: print the first 50 fibonacci.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int count;
+unsigned long fib1 = 0 , fib2 = 1 , sum;
+for (count = 0; count < 50; count++)
+{
+sum = fib1 + fib2;
+printf("%lu" , sum);
+fib1 = fib2;
+fib2 = sum;
+if (count == 49)
+printf("\n");
+else 
+printf(". ");
+}
+return (0);
+}
