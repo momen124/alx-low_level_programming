83920b9f7c915611d5b356513463dab21bac5eed
momen124 <55993326+momen124@users.noreply.github.com>
2023-10-11 00:34:58 +0300

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,23 +1,22 @@
 #include "main.h"
 
 /**
-* jack_bauer - prints every min of day
-*
-*
-* Return: always postive
-*/
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: void
+ */
 void jack_bauer(void)
 {
-int min ,h;
-for (h = 0 ; h <= 23; h++)
+int min, h;
+for (h = 0; h <= 23; h++)
 {
 for (min = 0; min <= 59; min++)
 {
-_putchar((h / 10) + 48);
-_putchar((h % 10) + 48);
+_putchar((h / 10) + '0');   
+_putchar((h % 10) + '0');
 _putchar(':');
-_putchar((min / 10) + 48);
-_putchar((min & 10) + 48);
+_putchar((min / 10) + '0');
+_putchar((min % 10) + '0');  
 _putchar('\n');
 }
 }
