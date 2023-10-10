632b85b7beb2abf86a116f6d98ae1a6cb979f7a0
momen124 <55993326+momen124@users.noreply.github.com>
2023-10-11 01:28:53 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,10 +11,10 @@ void print_times_table(int n)
 int prod , mult , num;
 if (n <= 15 && n >= 0)
 {
-for (num = 0 ; num <=n; num++)
+for (num = 0 ; num <= n; num++)
 {
 _putchar(48);
-for (mult = 1 ; mult <=n; mult++)
+for (mult = 1 ; mult <= n; mult++)
 {
 _putchar(',');
 _putchar(' ');
@@ -26,7 +26,7 @@ _putchar(' ');
 if (prod >= 100)
 {
 _putchar((prod / 100) + 48);
-_putchar((prod / 100) % 10 + 48);
+_putchar((prod / 10) % 10 + 48);
 }
 else if (prod <= 99 && prod >= 10)
 {
