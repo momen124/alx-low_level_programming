bc78a6781e8ec569fb465070540c78c44c630d17
momen124 <55993326+momen124@users.noreply.github.com>
2023-10-11 02:57:18 +0300

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,8 +8,8 @@
  */
 int main(void)
 {
-unsigned long fib1 = 0, fib2 = 1, sum = 0;  
-unsigned long total_sum = 0;  
+unsigned long fib1 = 0, fib2 = 1, sum = 0;
+unsigned long total_sum = 0;
 while (1)
 {
 sum = fib1 + fib2;
@@ -22,7 +22,6 @@ fib1 = fib2;
 fib2 = sum;
 }
 
-printf("%lu\n", total_sum);  
-
+printf("%lu\n", total_sum);
 return (0);
 }
