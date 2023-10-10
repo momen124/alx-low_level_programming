27dab7d7aa017957e9b2219dc969a4bafe77600f
momen124 <55993326+momen124@users.noreply.github.com>
2023-10-10 15:05:02 +0300

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * main - main function 
- *
- *
- * Return: Always 0.
- */
+* main - main function 
+*
+*
+* Return: Always 0.
+*/
 int main(void)
 {
 char str[] = "_putchar";
