c2234731f47b67b0f3a73e759768550fa732c756
momen124 <55993326+momen124@users.noreply.github.com>
2023-10-11 02:25:18 +0300

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,24 +2,30 @@
 
 /**
  * main - entry point
- * Description: print the sum of even.
+ * Description: print the sum of even Fibonacci numbers below 4,000,000.
  *
  * Return: Always 0
  */
 int main(void)
 {
-unsigned long fib1 = 0 , fib2 = 1 , sum;
-float total_sum;
-while (1)
-{
-sum = fib1 + fib2;
-if (sum > 4000000)
-break;
-if ((sum % 2) == 0)
-tota_sum += sum;
-fib1 = fib2;
-fib2 = sum;p
-printf("%.0f\n",total_sum);
-}
-return (0);
+    unsigned long fib1 = 0, fib2 = 1, sum = 0;  // Initialize sum to 0
+    unsigned long total_sum = 0;  // Initialize total_sum to 0 and change its type to unsigned long
+
+    while (1)
+    {
+        sum = fib1 + fib2;
+
+        if (sum > 4000000)
+            break;
+
+        if ((sum % 2) == 0)
+            total_sum += sum;
+
+        fib1 = fib2;
+        fib2 = sum;
+    }
+
+    printf("%lu\n", total_sum);  // Use %lu to print an unsigned long
+
+    return (0);
 }
