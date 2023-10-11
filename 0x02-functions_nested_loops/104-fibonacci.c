88f7f6c469718a92a90cfd5f34f053f092a797a3
momen124 <55993326+momen124@users.noreply.github.com>
2023-10-11 03:53:33 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h> 
+#include <stdio.h>
 
 /**
  * numLength - returns the length of a number
@@ -12,6 +12,7 @@ int numLength(unsigned long num)
 int length = 0;
 if (num == 0)
 return 1;
+
 while (num > 0)
 {
 num = num / 10;
@@ -24,7 +25,7 @@ return length;
  * main - Entry point
  *
  * Description: prints the first 98 Fibonacci numbers.
- * starting with 1 and 2, followed
+ * starting with 1 and 2, followed by a newline
  * Return: Always 0
  */
 int main(void)
@@ -35,18 +36,19 @@ unsigned long f1 = 1, f2 = 2, sum, max = 100000000, f1o = 0, f2o = 0, sumo = 0;
 for (count = 1; count <= 98; count++)
 {
 if (f1o > 0)
-printf("%lu", f1o);
+ printf("%lu", f1o);
 
-initial_zeros = numLength(max) - numLength(f1);
-while (f1o > 0 && initial_zeros > 0)
-{
-printf("%d", 0);
-initial_zeros--;
-}
+ initial_zeros = numLength(max) - numLength(f1);
+ while (f1o > 0 && initial_zeros > 0)
+ {
+ printf("%d", 0);
+ initial_zeros--;
+ }
 
-printf("%lu", f1);
-sum = (f1 + f2) % max;
-sumo = f1o + f2o + (f1 + f2) / max;
+ printf("%lu", f1);
+
+ sum = (f1 + f2) % max;
+ sumo = f1o + f2o + (f1 + f2) / max;
 
 f1 = f2;
 f1o = f2o;
