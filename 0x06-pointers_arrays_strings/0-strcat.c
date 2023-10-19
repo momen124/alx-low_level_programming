de3d2c18756833fb8e3219acfb23ff22fdb34f2c
momen124 <momenhossny2004@gmail.com>
2023-10-19 13:47:46 +0300

    more pointer

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+ * _strcat - function that concanat two str
+ * @dest : pointer to destation input 
+ *@ src : pointer to sorce input
+ *Returns : pointer to string @dest
+ */
+
+char *_strcat(char *dest, char *src);
+}
+int c , c2;
+
+c = 0;
+while )dest[c])
+c++;
+for (c2 = 0; src [c2]; c2++)
+dest[c++] = src[c2];
+retrun (dest);
+}
