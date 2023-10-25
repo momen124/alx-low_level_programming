b681220faa1453579f44e861c7a05fe26a50c70c
momen124 <momenhossny2004@gmail.com>
2023-10-25 18:31:04 +0300

    rev

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /*
- * rev_recursion - print the stirng inverse 
+ * rev_recursion - print the stirng inverse
  * @s: pointer to string
  *
  * Retrun void
  */
 void _print_rev_recursion(char *s)
 {
-if ( *s > '\0')
+if (*s > '\0')
 {
 _print_rev_recursion(s + 1);
 _putchar(*s);
