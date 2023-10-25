c72527efe37f01c67f31e390883d63b55684f7ba
momen124 <momenhossny2004@gmail.com>
2023-10-25 18:44:39 +0300

    rev

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#inculde "mian.h"
+
+/**
+* _strlen_recursion - return the length
+*@s: pointer to string 
+*
+*Return: void
+*/
+
+int _strlen_recursion(char *s)
+{
+int n = 0;
+
+if (*s > '\0')
+{
+n += _strlen_recursion(s +1) +1;
+}
+return(n);
+}
