6a60b6c5baa39a535999a4bb7643ed82d5ec7ff1
momen124 <momenhossny2004@gmail.com>
2023-10-26 02:38:36 +0300

    factionl

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*_pow_recursion - give the pow of x to y 
+*_pow_recursion - give the pow of x to y
 *@x: int
 *@y: int
 *
@@ -16,5 +16,5 @@ else if (y == 1)
 return (x);
 else if (y == 0)
 return (1);
-return (x * _pow recursion(x, y -1));
+return (x * _pow recursion(x, y - 1));
 }
