d86319c56f14f575d273e447d0517971eb0562eb
momen124 <momenhossny2004@gmail.com>
2023-10-25 20:32:25 +0300

    factorila

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#inculde "main.h"
+
+/**
+*int factorial - print factorial 
+*@n: int 
+*
+*Return: int
+*/
+
+int factorial(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+else if (n == 0)
+{
+return (1);
+}
+return (n * factorial (n-1));
+}
