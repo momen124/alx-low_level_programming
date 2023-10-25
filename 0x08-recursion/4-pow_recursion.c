b0d3c5f4e2bfc6ec51c513bb0c1b03efb3c1f246
momen124 <momenhossny2004@gmail.com>
2023-10-25 20:52:28 +0300

    co

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*_pow_recursion - give the pow of x to y 
+*@x: int
+*@y: int
+*
+*Return:int
+*/
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+else if (y == 1)
+return (x);
+else if (y == 0)
+return (1);
+return (x * _pow recursion(x, y -1));
+}
