8928d750e90b624432a95aae221aa5e74730d576
momen124 <momenhossny2004@gmail.com>
2023-10-25 18:35:36 +0300

    rev

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /*
- * _print_rev_recursion - prints the stirng revese 
+ *print_rev_recursion - Prints a string in reverse using recursion.
  * @s: pointer to string
  * Retrun void
  */
