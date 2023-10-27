a88dec2366b5746fc7cb320f49925511c5bd2d26
momen124 <55993326+momen124@users.noreply.github.com>
2023-10-28 01:48:26 +0200

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - print the program name, followed by a new line
- * @argc: argument count
- * @argv: argument vector
- * Return: 0
- */
+* main - Prints the program name
+* @argc: The number of command line arguments
+* @argv: The command line arguments
+*
+* Return: 0 (Success)
+*/
 int main(int argc, char *argv[])
 {
-    (void)argc; // Unused parameter
-    printf("%s\n", argv[0]);
-    return (0);
+if (argc > 0)
+{
+printf("%s\n", argv[0]);
+}
+return (0);
 }
