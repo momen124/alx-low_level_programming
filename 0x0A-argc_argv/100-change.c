b4b9f9532ab688e6d6c4fd71d5f15aa3b199e896
momen124 <55993326+momen124@users.noreply.github.com>
2023-10-27 21:53:21 +0200

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,38 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-/**
-* main - print the mini num of coins
-* @argc: num of commandline
-* @argv:pointer to array of command line
-* Retrun:0-success
-*/
 
+/**
+ * main - Print the minimum number of coins required to make change for an amount of money.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line arguments.
+ * Return: 0 for success, 1 for error.
+ */
 int main(int argc, char *argv[])
 {
-if (argc == 2)
-{
-	int i, minicent = 0, money = atoi(argv[1]);
+    if (argc != 2)
+    {
+        printf("Error\n");
+        return (1);
+    }
 
-	int cent[] = {25, 10, 5, 2, 1};
+    int i, minicent = 0, money = atoi(argv[1]);
+    int cent[] = {25, 10, 5, 2, 1};
 
-}
-for (i = 0; i < 5; i++)
-{
-	if (money >= cent[i])
-	{
-		minicent += money / cent[i];
-		money = money % cent[i];
-		if (money % cent[i] == 0)
-		{
-			break;
-		}
-	}
-	printf("%d\n", minicent);
-}
-else
-{
-	printf("Error\n");
-	return (1);
-}
-return (0);
+    if (money < 0)
+    {
+        printf("0\n");
+        return (0);
+    }
+
+    for (i = 0; i < 5; i++)
+    {
+        if (money >= cent[i])
+        {
+            minicent += money / cent[i];
+            money = money % cent[i;
+            if (money == 0)
+            {
+                break;
+            }
+        }
+    }
+
+    printf("%d\n", minicent);
+    return (0);
 }
