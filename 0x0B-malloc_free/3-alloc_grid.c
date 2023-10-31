19a7d1e1d4f9c38e4cd7ce7f1d15c5861b5a1713
momen124 <momenhossny2004@gmail.com>
2023-10-31 16:58:30 +0200

    malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,35 +1,41 @@
 #include "main.h"
 #include <stdlib.h>
-/**
-*alloc_grid - returns a pointer to a 2 dimensional array of intger 
-*@height:int.
-*@width:int
-*Return:2d array
-*/
 
-int **alloc_grid(int width, int height)
-{
-int **tab, i , j;
-tab = malloc(sizeof(*tab) * height);
-if (width < 0 || height < tab == 0)
-{
-    return (NULL);
-}    
-else 
-{
-    for(i = 0; i < height; i++)
-    {
-        tab[i] = malloc(sizeof(**tab) * width);
-        if (tab[i] == 0)
-        {
-            while (i--)
-            free(tab[i]);
-        free(tab);
+/**
+ * alloc_grid - returns a pointer to a 2-dimensional array of integers
+ * @width: width of the grid
+ * @height: height of the grid
+ * Return: a 2D array
+ */
+int **alloc_grid(int width, int height) {
+    int **tab, i, j;
+    
+    if (width < 0 || height < 0) {
         return (NULL);
-        }
-        for (j = 0; j < width; j++)
-        tab[i][j] = 0;
     }
-}
-return (tab);
+
+    tab = malloc(sizeof(*tab) * height);
+
+    if (tab == NULL) {
+        return (NULL); // Memory allocation failed
+    }
+
+    for (i = 0; i < height; i++) {
+        tab[i] = malloc(sizeof(**tab) * width);
+
+        if (tab[i] == NULL) {
+            while (i >= 0) {
+                free(tab[i]);
+                i--;
+            }
+            free(tab);
+            return (NULL); // Memory allocation for a row failed
+        }
+
+        for (j = 0; j < width; j++) {
+            tab[i][j] = 0;
+        }
+    }
+
+    return (tab);
 }
