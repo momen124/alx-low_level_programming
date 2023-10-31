9eb0fa90bf6d1eaf6d8e8aa41015b470f0ffa2e9
momen124 <momenhossny2004@gmail.com>
2023-10-31 16:54:25 +0200

    malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,7 +11,7 @@ int **alloc_grid(int width, int height)
 {
 int **tab, i , j;
 tab = malloc(sizeof(*tab) * height);
-if (width <= 0 || height <= tab == 0)
+if (width < = 0 || height <= tab == 0)
 {
     return (NULL);
 }    
