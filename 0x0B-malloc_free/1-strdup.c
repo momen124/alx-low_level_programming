532909d732e8356dc64264ebbb1b0ac924f7776c
momen124 <momenhossny2004@gmail.com>
2023-10-31 16:22:44 +0200

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,27 +1,40 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
-*_strdup - retrun a pointer to a newly allocated space in memory 
-*@str:string.
-*Return:0
+* str_concat - concatenates two strings
+* @s1: first string
+* @s2: second string
+*
+* Return: pointer to the new string, or NULL if it fails
 */
-
-char *_strdup(char *str)
+char *str_concat(char *s1, char *s2)
 {
-int i = 0, size = 0;
-char *m;
+char *concat;
+int i, j, len1 = 0, len2 = 0;
 
-if (str == NULL)
-retrun (NULL);
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 
-for (; str[size] != '\0'; size++);
-m = malloc(size * sizeof(*str)+ 1);
-if (m == 0)
+while (s1[len1])
+len1++;
+while (s2[len2])
+len2++;
+
+concat = malloc(sizeof(char) * (len1 + len2 + 1));
+
+if (concat == NULL)
 return (NULL);
-else
-{
-    for (; i < size; i++)
-    m[i] = str[i];
-}
-retrun (m);
+
+for (i = 0; i < len1; i++)
+concat[i] = s1[i];
+
+for (j = 0; j < len2; j++, i++)
+concat[i] = s2[j];
+
+concat[i] = '\0';
+
+return (concat);
 }
