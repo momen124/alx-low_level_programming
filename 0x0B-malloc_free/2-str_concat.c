e6152c6f4a87f5aabb2f1ab5c1af2abe905fc233
momen124 <momenhossny2004@gmail.com>
2023-10-31 16:15:53 +0200

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,46 +1,22 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+char *_strdup(char *str) {
+    if (str == NULL) {
+        return NULL;
+    }
 
-/**
-*_strlen - find lenght of a string 
-*@s:string
-*Return:int
-*/
+    int length = _strlen(str);
+    char *duplicate = (char *)malloc(length + 1); // +1 for the null terminator
 
-int _strlen(char *s)
-{
-    int size = 0;
-    for (; s[size] != '\0'; size++)
-    ;
-return (size);
+    if (duplicate == NULL) {
+        return NULL; // Memory allocation failed
+    }
 
-}
+    for (int i = 0; i < length; i++) {
+        duplicate[i] = str[i];
+    }
 
-/**
-*@s1:string 1
-*@s2: string 2
-*Retrun: pointer
-*/
-
-char *str_concat(char *s1, char *s2)
-{
- int size1, size2, i;
- char *m;
- if (!s1 || !s2)
- return NULL;
-size1 = _strlen(s1);
-size2 = _strlen(s2);
-m = malloc((size1 + size2 + 1)* sizeof(char));
-if (m == 0)
-return (0);
-
-for (i = 0; i<= size1 + size2; i++)
-{
-    if (i < size1)
-    m[i] = s1[i];
-else 
-m[i] = s2[i - size1]
-}
-m[i] = '\0'
-return (m);
+    duplicate[length] = '\0'; // Null-terminate the duplicated string
+    return duplicate;
 }
