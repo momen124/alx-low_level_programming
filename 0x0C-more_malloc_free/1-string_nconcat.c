6451f090e58f03bb204f09676a358def4261d72c
momen124 <momenhossny2004@gmail.com>
2023-11-02 20:48:15 +0200

    malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,34 +1,45 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
-*string_nconcat - cancatenates two string
-*@s1: pointer to 1st string
-*@s2: pointer to 2nd string
-*@n: num of bytes from n2 to concatenate
-*
-*Retrurn:pointer to space in memory containing 
-*/
-
+ * string_nconcat - concatenates two strings.
+ * @s1: pointer to the first string.
+ * @s2: pointer to the second string.
+ * @n: number of bytes from s2 to concatenate.
+ *
+ * Return: pointer to the concatenated string, or NULL on failure.
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
-
 {
     char *str;
     unsigned int i, j, s1_length, s2_length;
-    if (s1 == NULL || s2 == NULL)
-    return (NULL);
- for (s1_length = 0; s1[s1_length] !='\0'; s1_length++)
- ;
-for (s2_length = 0; s1[s2_length] !='\0'; s2_length++)
-;
-if ((str = malloc((s1_length + s2_length + 1))) == NULL)
-return (NULL);
-for (i = 0; s1[i] != '\0'; i++)
-str[i] = s1[i];
-for (j = 0; j <= n && s2[j] != '\0'; j++)
-{
-str[i++] = s2[j];
-}
-str[i] = '\0';
-return (str);
+
+    if (s1 == NULL)
+        s1 = "";
+    if (s2 == NULL)
+        s2 = "";
+
+    for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
+        ;
+
+    for (s2_length = 0; s2[s2_length] != '\0'; s2_length++)
+        ;
+
+    if (n >= s2_length)
+        n = s2_length;
+
+    if ((str = malloc(s1_length + n + 1)) == NULL)
+        return (NULL);
+
+    for (i = 0; s1[i] != '\0'; i++)
+        str[i] = s1[i];
+
+    for (j = 0; j < n; j++)
+    {
+        str[i++] = s2[j];
+    }
+
+    str[i] = '\0';
+    return (str);
 }
