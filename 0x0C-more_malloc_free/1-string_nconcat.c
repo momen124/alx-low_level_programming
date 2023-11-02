227cb2befe18b90858641493673c0086b41a4272
momen124 <momenhossny2004@gmail.com>
2023-11-02 20:53:33 +0200

    malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,34 +12,33 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *str;
-    unsigned int i, j, s1_length, s2_length;
+char *str;
+unsigned int i, j, s1_length, s2_length;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 
-    for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
-        ;
+for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
+;
 
-    for (s2_length = 0; s2[s2_length] != '\0'; s2_length++)
-        ;
+for (s2_length = 0; s2[s2_length] != '\0'; s2_length++)
+;
 
-    if (n >= s2_length)
-        n = s2_length;
+if (n >= s2_length)
+n = s2_length;
 
-    if ((str = malloc(s1_length + n + 1)) == NULL)
-        return (NULL);
+if ((str = malloc(s1_length + n + 1)) == NULL)
+return (NULL);
 
-    for (i = 0; s1[i] != '\0'; i++)
-        str[i] = s1[i];
+for (i = 0; s1[i] != '\0'; i++)
+str[i] = s1[i];
 
-    for (j = 0; j < n; j++)
-    {
-        str[i++] = s2[j];
-    }
-
-    str[i] = '\0';
-    return (str);
+for (j = 0; j < n; j++)
+{
+str[i++] = s2[j];
+}
+str[i] = '\0';
+return (str);
 }
