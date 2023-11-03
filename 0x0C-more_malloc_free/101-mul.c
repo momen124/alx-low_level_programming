d40a8975bc9937bb95cc6e7d40dd56de6870b2b7
momen124 <momenhossny2004@gmail.com>
2023-11-03 22:19:19 +0200

    more malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,73 +1,57 @@
+#include "main.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 /**
- * multiply - multiplies two positive numbers
- * @num1: first number
- * @num2: second number
- */
+* multiply - multiplies two positive numbers
+* @num1: first number
+* @num2: second number
+*/
 void multiply(char num1[], char num2[])
 {
-    int len1 = strlen(num1);
-    int len2 = strlen(num2);
-    int *result = calloc(len1 + len2, sizeof(int));
-    int i, j;
-    int start;
+int len1 = strlen(num1);
+int len2 = strlen(num2);
+int *result = calloc(len1 + len2, sizeof(int));
+int i, j;
+int start;
 
-    if (result == NULL) {
-        fprintf(stderr, "Memory allocation failed\n");
-        exit(1);
-    }
-
-    for (i = len1 - 1; i >= 0; i--) {
-        for (j = len2 - 1; j >= 0; j--) {
-            result[i + j + 1] += (num1[i] - '0') * (num2[j] - '0');
-            result[i + j] += result[i + j + 1] / 10;
-            result[i + j + 1] %= 10;
-        }
-    }
-
-    /* Print the result */
-    start = 0;
-    while (start < len1 + len2 - 1 && result[start] == 0) {
-        start++;
-    }
-
-    for (i = start; i < len1 + len2; i++) {
-        printf("%d", result[i]);
-    }
-    printf("\n");
-
-    free(result);
+for (i = len1 - 1; i >= 0; i--)
+{
+for (j = len2 - 1; j >= 0; j--)
+{
+result[i + j + 1] += (num1[i] - '0') * (num2[j] - '0');
+result[i + j] += result[i + j + 1] / 10;
+result[i + j + 1] %= 10;
+}
 }
 
-int isNumeric(const char *str) {
-    while (*str) {
-        if (*str < '0' || *str > '9') {
-            return 0; // Non-digit character found
-        }
-        str++;
-    }
-    return 1; // All characters are digits
+/* Print the result */
+start = result[0] == 0 ? 1 : 0;
+
+for (i = start; i < len1 + len2; i++)
+printf("%d", result[i]);
+printf("\n");
+
+free(result);
 }
 
+/**
+* main - multiplies two positive numbers
+* @argc: argument count
+* @argv: argument vector
+* Return: int
+*/
 int main(int argc, char *argv[])
 {
-    if (argc != 3) {
-        fprintf(stderr, "Error\n");
-        return 98;
-    }
+if (argc != 3)
+{
+printf("Error\n");
+exit(98);
+}
 
-    char *num1 = argv[1];
-    char *num2 = argv[2];
+multiply(argv[1], argv[2]);
 
-    if (!isNumeric(num1) || !isNumeric(num2)) {
-        fprintf(stderr, "Error\n");
-        return 98;
-    }
-
-    multiply(num1, num2);
-
-    return 0;
+return (0);
 }
