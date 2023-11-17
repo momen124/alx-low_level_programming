f058cf3961853846f7bdea2fedd904976cbbe9e7
momen124 <momenhossny2004@gmail.com>
2023-11-17 18:28:58 +0200

    linked

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,19 @@
 section .data
-    hello db 'Hello, Holberton', 0
-
+   msg: db "Hello, Holberton", 0
+   fmt: db "%s", 10, 0
 section .text
     global main
     extern printf
 
 main:
-    ; Prepare the arguments for printf
-    mov rdi, hello     ; The format string
-    xor rax, rax       ; 0 floating point parameters
-    ; Call printf
-    call printf
-    ; Return 0
-    mov eax, 0x60      ; The syscall number for sys_exit
-    xor edi, edi       ; Exit code 0
-    syscall            ; Call the kernel
+       push rbp
+
+   mov rdi,fmt
+   mov rsi,msg
+   mov rax,0
+   call printf
+
+   pop rbp
+
+   mov rax,0
+   ret
