56dbabd93aacbc9b4e5f04f714fad0712aa6fd8c
momen124 <momenhossny2004@gmail.com>
2023-11-21 22:49:18 +0200

    more_linked

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,25 +1,22 @@
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * print_list - prints all the elements of a linked list
- * @h: pointer to the list_t list to print
- *
- * Return: the number of nodes printed
- */
-size_t print_listint(const list_t *h)
+* print_listint - prints all the elements of a listint_t list
+* @h: pointer to the start of the list
+*
+* Return: number of nodes in the list
+*/
+size_t print_listint(const listint_t *h)
 {
-	size_t i = 0;
+size_t nodes = 0;
 
-	while (h)
-	{
-		if (!h->int)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", h->len, h->int);
-		h = h->next;
-		i++;
-	}
+while (h)
+{
+nodes++;
+printf("%d\n", h->n);
+h = h->next;
+}
 
-	return (i);
+return (nodes);
 }
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,22 +1,46 @@
-#include "list.h"
-#include <stdio.h>
+#include "lists.h"
 #include <stdlib.h>
 
 /**
- * listin_len print all the element in  a linked list 
- * @h:pointer to the list 
- * 
- * Return: the number in hte list 
+* delete_nodeint_at_index - deletes the node at index
+* index of a listint_t linked list
+* @head: double pointer to the start of the list
+* @index: index of the node that should be deleted, starts at 0
+*
+* Return: 1 if it succeeded, -1 if it failed
 */
-
-
-size_t listint_len(const listint_t *h)  
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-   size_t count = 0;   
-   while (h !=NULL)
-   {
-     count++;
-     h= h->next;
-   }
-   return count;
+listint_t *temp;
+listint_t *next;
+unsigned int i;
+
+if (head == NULL || *head == NULL)
+{
+return (-1);
+}
+
+temp = *head;
+
+if (index == 0)
+{
+*head = temp->next;
+free(temp);
+return (1);
+}
+
+for (i = 0; i < index - 1; i++)
+{
+if (temp == NULL || temp->next == NULL)
+{
+return (-1);
+}
+temp = temp->next;
+}
+
+next = temp->next->next;
+free(temp->next);
+temp->next = next;
+
+return (1);
 }
