05a28490b647b791433598789dbcefeefdd06d87
momen124 <momenhossny2004@gmail.com>
2023-11-27 18:30:34 +0200

    filei/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,34 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
 
-char* read_textfile(const char* filename) {
-    FILE* file = fopen(filename, "r");
-    if (!file) {
-        perror("Error opening file");
-        exit(1);
+#define READ_BUF_SIZE 1024
+
+/*
+*read_textfile - read txt and print it in POSIX
+*@filename: name of file to read
+*@letters:num of byter to read
+*Return: num of byter read/printer
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    int f;
+    ssize_t bytes;
+    char buffer[READ_BUF_SIZE * 8];
+
+    if (!filename || !letters)
+        return (0);
+
+    f = open(filename, O_RDONLY);
+    if (f == -1)
+        return (0);
+
+    bytes = read(f, &buffer[0], letters);
+    if (bytes == -1)
+    {
+        close(f);
+        return (-1);
     }
 
-    fseek(file, 0, SEEK_END);
-    long size = ftell(file);
-    rewind(file);
-
-    char* buffer = malloc(size + 1);
-    if (!buffer) {
-        perror("Error allocating memory");
-        exit(1);
+    bytes = write(STDOUT_FILENO, &buffer[0], letters);
+    if (bytes == -1)
+    {
+        close(f);
+        return (-1);
     }
 
-    fread(buffer, 1, size, file);
-    buffer[size] = '\0';
-
-    fclose(file);
-    return buffer;
-}
-
-int main() {
-    const char* filename = "test.txt";
-    char* text = read_textfile(filename);
-    printf("%s\n", text);
-    free(text);
-    return 0;
+    close(f);
+    return (bytes);
 }
